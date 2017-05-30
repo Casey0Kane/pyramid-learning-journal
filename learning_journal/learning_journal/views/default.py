7da7e60d034914b733163022ca69d8a01da6d113
Casey O'Kane <okanecasey@gmail.com>
2017-05-30 15:56:47 -0700

    working on deploying to heroku

--- learning_journal/learning_journal/views/default.py
+++ learning_journal/learning_journal/views/default.py
@@ -3,36 +3,32 @@ from pyramid.response import Response
 import io
 import os
 
-HERE = os.path.dirname(__file__)
+HERE = os.path.dirname(os.path.dirname(__file__))
 
 
 def list_view(request):
     """View for the list of journal entries."""
-    with io.open(os.path.join(HERE, 'index.html')) as the_file:
+    with io.open(os.path.join(HERE, 'templates/index.html')) as the_file:
         imported_text = the_file.read()
-        the_file.close()
         return Response(imported_text)
 
 
 def detail_view(request):
     """View for a single journal entry."""
-    with io.open(os.path.join(HERE, 'entry.html')) as the_file:
+    with io.open(os.path.join(HERE, 'templates/entry.html')) as the_file:
         imported_text = the_file.read()
-        the_file.close()
         return Response(imported_text)
 
 
 def create_view(request):
     """Create a new view."""
-    with io.open(os.path.join(HERE, 'new-entry.html')) as the_file:
+    with io.open(os.path.join(HERE, 'templates/new-entry.html')) as the_file:
         imported_text = the_file.read()
-        the_file.close()
         return Response(imported_text)
 
 
 def update_view(request):
     """For updating an existing view."""
-    with io.open(os.path.join(HERE, 'edit-entry.html')) as the_file:
+    with io.open(os.path.join(HERE, 'templates/edit-entry.html')) as the_file:
         imported_text = the_file.read()
-        the_file.close()
         return Response(imported_text)
