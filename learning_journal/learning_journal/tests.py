4ef4fd3d7e4685ee3e28bef5e9776e73ea74ebab
Casey O'Kane <okanecasey@gmail.com>
2017-05-31 12:41:37 -0700

    added link to site in read me and fixed tests.

--- learning_journal/learning_journal/tests.py
+++ learning_journal/learning_journal/tests.py
@@ -6,17 +6,17 @@ import pytest
 @pytest.fixture
 def list_view_response():
     """Return a response for a home page."""
-    from expense_tracker.views.default import list_view
+    from learning_journal.views.default import list_view
     request = testing.DummyRequest()
     response = list_view(request)
     return response
 
 
-def test_list_view_returns_response_given_request(list_view):
+def test_list_view_returns_response_given_request(list_view_response):
     """Home view returns a response object when given a request."""
-    assert isinstance(list_view, Response)
+    assert isinstance(list_view_response, Response)
 
 
-def test_list_view_is_good(list_view):
+def test_list_view_is_good(list_view_response):
     """Test that the home view is 200 response OK."""
-    assert list_view.status_code == 200
+    assert list_view_response.status_code == 200
